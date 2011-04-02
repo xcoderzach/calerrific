070d895ff92e3c472736e1556bada4229a1b2f8f
Brian Goslinga <gosli008@morris.umn.edu>
2011-04-02 13:45:19 -0500

    Search for tag by id

--- cake/app/controllers/tags_controller.php
+++ cake/app/controllers/tags_controller.php
@@ -3,5 +3,18 @@ class TagsController extends AppController {
 
 	var $name = 'Tags';
 
+	function index() {
+	  $this->view = 'Json';
+	 
+	  $id = $this->params['url']['id'];
+	  $params = array('conditions' => array('id' => $id));
+	  $raw = $this->Tag->find('first', $params);
+	  if ($raw) {
+		$this->set('json', $raw['Tag']['name']);
+	  } else {
+		$this->set('json', false);
+	  }
+	}
+
 }
 ?>
