1a14294893da11de02cecf109ce88e47d76b0c11
Brian Goslinga <gosli008@morris.umn.edu>
2011-04-02 13:26:13 -0500

    Search for user by id

--- cake/app/controllers/users_controller.php
+++ cake/app/controllers/users_controller.php
@@ -1,7 +1,30 @@
 <?php
 class UsersController extends AppController {
 
-	var $name = 'Users';
+  var $name = 'Users';
+
+	function index() {
+	  $this->view = "Json";
+
+	  $conditions = array('id' => $this->params['url']['id']);
+	  $params = array('conditions' => $conditions);
+	  $raw = $this->User->find('first', $params);
+	  $raw = $raw['User'];
+
+	  $fields = array('name', 'username', 'position', 'email', 'title',
+					  'department');
+	  $this->set('json', $this->_extract_fields($raw, $fields));
+	}
+
+	function _extract_fields($obj, $fields) {
+	  $res = array();
+	  foreach ($fields as $field) {
+		if (isset($obj[$field]) && $obj[$field] != '') {
+		  $res[$field] = $obj[$field];
+		}
+	  }
+	  return $res;
+	}
 
 }
 ?>
